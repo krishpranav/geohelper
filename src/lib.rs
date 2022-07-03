adc30d4bebf08cffc38097bb143a6870659d6cfa
krishpranav <krisna.pranav@gmail.com>
2022-07-03 17:51:36 +0530

    src: lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+// #![deny(missing_docs)]
+mod formula;
+
+pub use formula::Distance;
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Location(f64, f64);
 
 impl Location {
@@ -14,5 +21,39 @@ impl Location {
     }
 
     pub fn distance_to(&self, to: &Location) -> Result<Distance, String> {
+        match formula::vincenty_inverse(self, to, 0.00001, 0.0) {
+            Ok(res) => Ok(res.distance),
+            Err(e) => Err(e),
+        }
     }
+
+    pub fn haversine_distance_to(&self, to: &Location) -> Distance {
+        formula::haversine_distance_to(self, to)
+    }
+
+    pub fn is_in_circle(&self, center: &Location, radius: Distance) -> Result<bool, String> {
+        match formula::vincenty_inverse(self, center, 0.00001, 0.0) {
+            Ok(res) => Ok(res.distance.meters() < radius.meters()),
+            Err(e) => Err(e),
+        }
+    }
+
+    pub fn center(coords: &[&Location]) -> Location {
+        formula::center_of_coords(coords)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_distance_haversine() {
+        let l1 = Location::new(27.740068, 85.337576);
+        let l2 = Location::new(27.740286, 85.337059);
+
+        let distance = l1.haversine_distance_to(&l2);
+        assert_eq!(distance.meters(), 56.36);
+    }
+
 }
